bf5dafefa78066f2a9d5408c11dd402b419fcd78
Mike Pennisi <mike@mikepennisi.com>
2015-08-26 16:32:33 -0400

    Introduce Vagrant

--- test/AEFrameworkTestCase.php
+++ test/AEFrameworkTestCase.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'api');
+define('DB_PASSWORD', 'bocoup');
 define('DB_DATABASE', 'api');
 
 /**
